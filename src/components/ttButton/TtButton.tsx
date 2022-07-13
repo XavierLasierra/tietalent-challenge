71ae95a634fd5e3828f9525fac67d59bdd953717
Xavier Lasierra <xlp1998@gmail.com>
2022-07-13 07:33:51 +0200

    Improve search functionality
    - Crete TtInput component.
    - Add current search query as search default value.
    - Enhance TtButton component.

--- src/components/ttButton/TtButton.tsx
+++ src/components/ttButton/TtButton.tsx
@@ -1,28 +1,41 @@
-import React, { ForwardedRef } from "react";
+import React, { ButtonHTMLAttributes, ForwardedRef } from "react";
 import styles from "./TtButton.module.scss";
 
 export interface TTButtonProps {
   href?: string;
   text: string;
   onClick?: () => void;
-  type?: "primary" | "secondary";
+  type?: "primary" | "secondary" | "transparent";
   size?: "small" | "large";
+  buttonType?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
+  width?: "auto" | "fixed";
 }
 
 const TtButton = (
-  { href, text, onClick, type = "primary", size = "small" }: TTButtonProps,
+  {
+    href,
+    text,
+    onClick,
+    type = "primary",
+    size = "small",
+    buttonType = "button",
+    width = "fixed",
+  }: TTButtonProps,
   ref: ForwardedRef<HTMLAnchorElement>
 ) => {
   const getButtonStyle = () => {
     let buttonStyle = [styles.button];
     if (type === "secondary") buttonStyle.push(styles.button_secondary);
+    else if (type === "transparent")
+      buttonStyle.push(styles.button_transparent);
+    if (width === "auto") buttonStyle.push(styles.button_auto);
     return buttonStyle.join(" ");
   };
 
   const getTextStyle = () => {
     let textStyle = [styles.buttonText];
     if (size === "large") textStyle.push(styles.buttonText_large);
-
+    if (type === "transparent") textStyle.push(styles.buttonText_transparent);
     return textStyle.join(" ");
   };
 
@@ -31,7 +44,7 @@ const TtButton = (
       <span className={getTextStyle()}>{text}</span>
     </a>
   ) : (
-    <button className={getButtonStyle()} onClick={onClick}>
+    <button type={buttonType} className={getButtonStyle()} onClick={onClick}>
       <span className={getTextStyle()}>{text}</span>
     </button>
   );
